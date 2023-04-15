b985f24b9bf2db1ab7fdbce8db6c62914f72bc02
Darshan Bennur <bennurdarshan@gmail.com>
2023-04-15 23:14:18 +0530

    added mentorApplication and Pricing Section

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const flash = require('connect-flash');
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, "./uploads")
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${UserController.session.id}-${Date.now()}-${file.originalname}`)
+    },
+});
+
+const upload = multer({ storage })
 
 const UserController = require('./Controllers/user_controller');
 const Blog_Controller = require('./Controllers/blog_controller')
 const Featured_Controller = require('./Controllers/featured_controller')
 const ContactUs_Controller = require('./Controllers/contactUs_controller')
 const MarketTerm_Controller = require('./Controllers/marketTerm_controller')
+const MentorApplication_Controller = require('./Controllers/mentorApplication_controller')
 
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
@@ -19,8 +32,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended : false}))
-app.use(flash())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
     res.locals.isLoggedIn = UserController.session.isSigned;
@@ -52,27 +64,35 @@ app.get('/simulator', (req, res) => {
     res.render('simulator');
 })
 
+app.get('/mentorApplication', (req, res) => {
+    res.render('mentorApplication',{status : ""});
+})
+
+app.post('/postMentorApplication', upload.single('certificate'),MentorApplication_Controller.postMentorApplication)
+
+// console.log("Path : " + req.file.path);
+
 app.get('/profile', (req, res) => {
-    res.render('profile',{
-        name : UserController.session.name,
-        email : UserController.session.email,
-        isSignedIn : UserController.session.isSigned,
-        education : UserController.session.education,
-        profileImage : UserController.session.profileImage,
-        countryCode : UserController.session.countryCode,
-        phoneNumber : UserController.session.phoneNumber,
-        income : UserController.session.income,
-        incomeType : UserController.session.incomeType,
-        isMentor : false
+    res.render('profile', {
+        name: UserController.session.name,
+        email: UserController.session.email,
+        isSignedIn: UserController.session.isSigned,
+        education: UserController.session.education,
+        profileImage: UserController.session.profileImage,
+        countryCode: UserController.session.countryCode,
+        phoneNumber: UserController.session.phoneNumber,
+        income: UserController.session.income,
+        incomeType: UserController.session.incomeType,
+        isMentor: false
     });
 })
 
 app.post('/changesProfile', UserController.makeChanges)
 
 app.get('/signIn', (req, res) => {
-    res.render('signIn',{
-        errr:'',
-        loginError : ''
+    res.render('signIn', {
+        errr: '',
+        loginError: ''
     });
 })
 
@@ -87,18 +107,18 @@ app.get('/logout', (req, res) => {
 
 app.get('/marketTerm', MarketTerm_Controller.getAllFAQ)
 
-app.get('/aboutUs', async(req, res) => {
+app.get('/aboutUs', async (req, res) => {
     res.render('about');
 })
 
-app.get('/pricing', async(req, res) => {
+app.get('/pricing', async (req, res) => {
     res.render('pricing');
 })
 
-app.get('/contactUs', async(req, res) => {
+app.get('/contactUs', async (req, res) => {
     res.render('contactUs', {
-        name : UserController.session.name,
-        email : UserController.session.email
+        name: UserController.session.name,
+        email: UserController.session.email
     });
 })
 
@@ -119,7 +139,7 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, err => {
-    if(err) {
+    if (err) {
         console.log(err);
     }
     else
